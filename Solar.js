bc415e5a82694aa801dcc44431801452bd4b3df5
Just-557 <jabrown557@gmail.com>
2024-10-01 10:16:04 -0700

    Update Solar.js

--- Solar.js
+++ Solar.js
@@ -4,3 +4,15 @@ const= canvas.getContext('2d')
 
 canvas.width = window.outerwidth
 canvas.height = window.outerheight
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'f') { // Press 'f' to toggle full-screen
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen();
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            }
+        }
+    }
+});
